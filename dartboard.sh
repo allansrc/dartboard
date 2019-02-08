41ee717783b18e9efc188796ab115dbd395b2e3b
Allan Ramos <allanramos.teixeira@gmail.com>
2019-02-08 00:49:46 -0300

    Update dartboard.sh

--- dartboard.sh
+++ dartboard.sh
@@ -1,9 +1,12 @@
 sudo apt install git
 ///
-sudo sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
-sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
-sudo apt-get update
-sudo apt-get install dart
+$ sudo apt-get update
+$ sudo apt-get install apt-transport-https
+$ sudo sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+$ sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+$ sudo apt-get update
+$ sudo apt-get install dart
+
 // java
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
